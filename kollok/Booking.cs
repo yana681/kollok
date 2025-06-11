b3b5c4dc1d55cacfead9f7dc7ca8e0ceeb4150b5
яна кайтукова <yanakaytuk@gmail.com>
2025-06-11 19:08:28 +0300

    создание класса для бронирования техники

--- kollok/Booking.cs
+++ kollok/Booking.cs
@@ -8,6 +8,22 @@ namespace kollok
 {
     class Booking
     {
+        public Equipment Equipment { get; set; }
+        public string ForemanName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
 
+        public Booking(Equipment equipment, string foreman, DateTime start, DateTime end)
+        {
+            Equipment = equipment;
+            ForemanName = foreman;
+            StartDate = start;
+            EndDate = end;
+        }
+
+        public override string ToString()
+        {
+            return $"{Equipment.Type} {Equipment.Brand}, {ForemanName}, с {StartDate.ToShortDateString()} по {EndDate.ToShortDateString()}";
+        }
     }
 }
